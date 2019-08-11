9e2cfc210b165df2dd1ee636037cced06042729f
gamkartiev <gamkartiev@yandex.ru>
2019-08-11 07:27:13 +0300

    some change in css - background

--- views/articles.php
+++ views/articles.php
@@ -4,8 +4,9 @@
 <head>
  <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
  <title>Мой первый блог</title>
- <link rel="stylesheet" type="text/css" href="views/css/style.css">
+
  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+ <link rel="stylesheet" type="text/css" href="views/css/style.css">
 </head>
 
 <body>
