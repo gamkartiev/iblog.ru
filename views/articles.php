db234186a22c9bf05e1b4afdc8f0ed8e621fffbc
gamkartiev <gamkartiev@yandex.ru>
2019-08-16 07:25:50 +0300

    only some correction

--- views/articles.php
+++ views/articles.php
@@ -30,31 +30,31 @@
   </section>
 
 
-<?php 
-for ($i = 1; $i <= $total; $i++){
-  echo "<a href=index.php?page=".$i."> Страница ".$i." </a>";
-}
- ?>
-
-
 <!-- <?php 
+for ($i = 1; $i <= $total; $i++){
+  echo  "<a href=index.php?page=".$i."> Страница ".$i." </a>";
+}
+ ?> -->
+
+
+<?php 
 // Проверяем нужны ли стрелки назад 
-if ($page != 1) $pervpage = '<a href= ./page?page=1><<</a> 
-                               <a href= ./page?page='. ($page - 1) .'><</a> '; 
+if ($page != 1) $pervpage = '<a href=index.php?page=1><<</a> 
+                               <a href=index.php?page='. ($page - 1) .'><</a> '; 
 // Проверяем нужны ли стрелки вперед 
-if ($page != $total) $nextpage = ' <a href= ./page?page='. ($page + 1) .'>></a> 
-                                   <a href= ./page?page=' .$total. '>>></a>'; 
+if ($page != $total) $nextpage = ' <a href=index.php?page='. ($page + 1) .'>></a> 
+                                   <a href=index.php?page=' .$total. '>>></a>'; 
 
 // Находим две ближайшие станицы с обоих краев, если они есть 
-if($page - 2 > 0) $page2left = ' <a href= ./page?page='. ($page - 2) .'>'. ($page - 2) .'</a> | '; 
-if($page - 1 > 0) $page1left = '<a href= ./page?page='. ($page - 1) .'>'. ($page - 1) .'</a> | '; 
-if($page + 2 <= $total) $page2right = ' | <a href= ./page?page='. ($page + 2) .'>'. ($page + 2) .'</a>'; 
-if($page + 1 <= $total) $page1right = ' | <a href= ./page?page='. ($page + 1) .'>'. ($page + 1) .'</a>'; 
+if($page - 2 > 0) $page2left = ' <a href=index.php?page='. ($page - 2) .'>'. ($page - 2) .'</a> | '; 
+if($page - 1 > 0) $page1left = '<a href=index.php?page='. ($page - 1) .'>'. ($page - 1) .'</a> | '; 
+if($page + 2 <= $total) $page2right = ' | <a href=index.php?page='. ($page + 2) .'>'. ($page + 2) .'</a>'; 
+if($page + 1 <= $total) $page1right = ' | <a href=index.php?page='. ($page + 1) .'>'. ($page + 1) .'</a>'; 
 
 // Вывод меню 
 echo $pervpage.$page2left.$page1left.'<b>'.$page.'</b>'.$page1right.$page2right.$nextpage; 
 
-?> -->
+?> 
 </main>
 
 
