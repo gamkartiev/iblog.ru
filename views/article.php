8a106c9f2a702fc367f53ab572cb23335fd2af09
gamkartiev <gamkartiev@yandex.ru>
2019-08-16 07:36:23 +0300

    some change in style.css

--- views/article.php
+++ views/article.php
@@ -4,13 +4,13 @@
 <head>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
     <title>Мой первый блог</title>
-    <link rel="stylesheet" type="text/css" href="/views/css/style.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/views/css/style.css">
 </head>
 <body>
 
 <header>
-<?php include "heder.php" ?>
+<?php include "header.php" ?>
   
 </header>
 
