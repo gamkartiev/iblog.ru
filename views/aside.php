e4ceefa8f46dff42a2a2987fe68f4b7291193b09
gamkartiev <gamkartiev@yandex.ru>
2020-04-12 12:52:10 +0300

    some changes

--- views/aside.php
+++ views/aside.php
@@ -12,7 +12,11 @@
 
 <aside class="last_comments">
 <div class="block_name"> Последние комментарии </div>
-<div class="block_body"> </div>
+<div class="block_body">
+  <?php foreach($show_title as $value): ?>
+  <li class="list_aside"><a class="link_aside" href="article.php?id=<?=$value['id']?>"><?=$value['title']?></a></li>
+  <?php endforeach ?>
+</div>
 </aside>
 
 <script> document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>  <!--код для расширения livereload - автоматического обновления страницы сайта после сохранения кода -->
